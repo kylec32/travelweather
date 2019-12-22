3aa47a377cbb0b9da41fc16ca039309e209d867b
Kyle Carter <kylec32@gmail.com>
2019-12-22 08:55:02 -0700

    Intial work on function

---
+++ functions/forecast/forecast.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+const KEY = process.env.DARK_SKY_KEY;
+
+exports.handler = function(event, context, callback) {
+    let url = `https://api.darksky.net/forecast/${KEY}/${event.queryStringParameters.latitude},${event.queryStringParameters.longitude}`;
+
+    (async () => {
+        let response = await fetch(url);
+        let jsonResponse = await response.json();
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(jsonResponse)
+        });
+    })();
+
+    
+}
