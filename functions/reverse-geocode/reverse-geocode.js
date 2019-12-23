6ae1ca0668d08eb346642269e9030df2062ce6ad
Kyle Carter <kylec32@gmail.com>
2019-12-22 22:22:25 -0700

    Got reverse geocode working I believe

--- functions/reverse-geocode/reverse-geocode.js
+++ functions/reverse-geocode/reverse-geocode.js
@@ -2,15 +2,14 @@ const fetch = require('node-fetch');
 
 const KEY = process.env.HERE_KEY;
 
-exports.handler = function(event, context, callback) {
-    let url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=${event.queryStringParameters.latitude}%2C${event.queryStringParameters.longitude}%2C1500&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=${KEY}`
+exports.handler = async function(event, context, callback) {
+    let url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=${event.queryStringParameters.latitude}%2C${event.queryStringParameters.longitude}%2C3000&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=${KEY}`
 
-    (async () => {
-        let response = await fetch(url);
-        let jsonResponse = await response.json();
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(jsonResponse)
-        });
-    })();  
+    let response = await fetch(url);
+    let jsonResponse = await response.json();
+    let address = jsonResponse.Response.View[0].Result[0].Location.Address;
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({'city': address.City, 'State': address.State})
+    });
 }
